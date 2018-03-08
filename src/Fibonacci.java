3572dbbacf7d07228be763a19fa9ce9912db9da4
silence9510 <1016186751@qq.com>
2018-03-08 21:28:30 +0800

    fibonacci

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,8 +1,8 @@
 public class Fibonacci {
-    private int[] fib = new int[40];
+    private int[] fib = new int[200];
 
     public Fibonacci() {
-        fib[0] = 1;
+        fib[0] = 0;
         fib[1] = 1;
         for(int i=2; i<fib.length; i++){
             fib[i] = fib[i-1] + fib[i-2];
@@ -14,6 +14,6 @@ public class Fibonacci {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Fibonacci().returnN(10));
+        System.out.println(new Fibonacci().returnN(30));
     }
 }
